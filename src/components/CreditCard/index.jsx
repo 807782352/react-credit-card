d40b382d069f4fb3d624004be594d42850564bfa
z297xu <kyrie807782352@gmail.com>
2023-08-22 16:28:59 -0400

    add HTML structure of the credit-card component

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -1,16 +1,63 @@
 import React from 'react'
-import Chip from '../../images/chip.webp'
+import Chip from '../../images/chip.png'
 import Visa from '../../images/visa.png'
+import './index.css'
 
-export default function CreditCard() {
-  return (
-    <div className='credit-card'>
-        <div className="wrapper">
-            <div className="branding">
-                <img className='chip-img' src={Chip} alt="Chip" />
-                <img className='visa-img' src={Visa} alt="Visa" />
+export default function CreditCardFront() {
+    return (
+        <div className='credit-card'>
+            <div className="container container__credit-card">
+                <div className="card-front">
+                    <div className="branding">
+                        <img className='chip-img' src={Chip} alt="Chip" />
+                        <img className='visa-img' src={Visa} alt="Visa Logo" />
+                    </div>
+                    <div className="card-number">
+                        <div>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                        </div>
+                        <div>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                        </div>
+                        <div>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                        </div>
+                        <div>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                            <span className="card-number-display">_</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-details">
+                    <div>
+                        <span>Card Holder</span>
+                        <span id='card-holder-name'>Your Name</span>
+                    </div>
+                    <div>
+                        <span>Expiration Date</span>
+                        <span id='validity'>xx/xx</span>
+                    </div>
+                </div>
+                <div className="card-back">
+                    <div className="black-strip"></div>
+                    <div className="white-strip">
+                        <span>CVV</span>
+                        <div id="cvv-display"></div>
+                    </div>
+                    <img src={Visa} alt="Visa Logo" />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
